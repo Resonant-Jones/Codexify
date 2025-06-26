088f12fc4073c30eb05606dfc765ca20fc68841d
CodeSandbox <support@codesandbox.io>
2025-06-26 03:28:28 +0000

    🔒 Snapshot: All system tests passing, full agent/plugin infrastructure stable

---
+++ guardian/config/__init__.py
@@ -0,0 +1,9 @@
+"""
+Guardian Configuration Package
+----------------------------
+Configuration management for the Guardian system.
+"""
+
+from .system_config import system_config
+
+__all__ = ['system_config']
