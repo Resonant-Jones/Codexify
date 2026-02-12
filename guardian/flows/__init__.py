8b31ef87951a606b32825e1427f7924e3c74d2dd
Resonant Jones <jones@resonantconstructs.ai>
2026-02-12 13:29:18 -0500

    TASK-2026-02-12-003_primitive_contracts_and_registry: primitive contracts + registry

--- guardian/flows/__init__.py
+++ guardian/flows/__init__.py
@@ -8,6 +8,7 @@ from guardian.flows.spec import (
     FlowStepResult,
     PrimitiveName,
 )
+from guardian.flows.primitives import PrimitiveRegistry, export_primitive_catalog
 
 __all__ = [
     "FLOW_SPEC_VERSION",
@@ -16,4 +17,6 @@ __all__ = [
     "FlowStep",
     "FlowStepResult",
     "PrimitiveName",
+    "PrimitiveRegistry",
+    "export_primitive_catalog",
 ]
